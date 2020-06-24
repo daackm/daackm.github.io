86cc7a30cd1798754d9a70f48849b833a6779485
daackm <59622612+daackm@users.noreply.github.com>
2020-06-24 18:39:58 -0500

    Add files via upload

--- Blinking_LEDs.ino
+++ Blinking_LEDs.ino
@@ -4,15 +4,19 @@ int kounter=0;  //Ignore the rest of this line
 void setup()
    {
     Serial.begin(9600);
+    pinMode(2,OUTPUT);
+    pinMode(7,OUTPUT);
+    pinMode(8,OUTPUT);
    }
 
 void loop()
    {
     kounter=kounter+1;
-    digitalWrite(2,HIGH);
-    digitalWrite(3,LOW);
+    digitalWrite(7,HIGH);
+    digitalWrite(8,LOW);
     delay(1000);
-    digitalWrite(2,LOW);
-    digitalWrite(3,HIGH);
+   
+    digitalWrite(7,LOW);
+    digitalWrite(8,HIGH);
     delay(1000);
    }
